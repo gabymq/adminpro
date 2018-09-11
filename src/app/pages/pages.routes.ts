0e2d3d99cc628942dbd26ecf8ef0c58ac18797d7
gabymq <gmorenogaby@gmail.com>
2018-09-11 12:23:06 -0500

    cambiar colores en el localStorage

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -3,6 +3,7 @@ import { PagesComponent } from "./pages.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { ProgressComponent } from "./progress/progress.component";
 import { Graficas1Component } from "./graficas1/graficas1.component";
+import { AccountSettingsComponent } from "./account-settings/account-settings.component";
 
 
 
@@ -15,6 +16,8 @@ const pagesRoutes: Routes = [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'progress', component: ProgressComponent },
       { path: 'graficas1', component: Graficas1Component },
+      { path: 'account-settings', component: AccountSettingsComponent },
+
       // la ruta cuando no exite ninguna ruta, se redirecciona al dashboard
       { path: 'l', redirectTo: '/dashboard', pathMatch: 'full' },
    ]
